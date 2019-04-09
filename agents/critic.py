5d3cc2c2b9bac7f230d15f1558963e16fa8549ab
Trey Bean <trey.bean@icloud.com>
2019-04-09 10:31:10 -0600

    Removed double activation on critic and added l2 regularizers

--- agents/critic.py
+++ agents/critic.py
@@ -1,4 +1,4 @@
-from keras import layers, models, optimizers
+from keras import layers, models, optimizers, regularizers
 from keras import backend as K
 
 
@@ -27,18 +27,26 @@ class Critic:
         actions = layers.Input(shape=(self.action_size,), name="actions")
 
         # Add hidden layer(s) for state pathway
-        net_states = layers.Dense(units=32)(states)
+        net_states = layers.Dense(units=32, kernel_regularizer=regularizers.l2(0.01))(
+            states
+        )
         net_states = layers.BatchNormalization()(net_states)
         net_states = layers.Activation("relu")(net_states)
-        net_states = layers.Dense(units=64, activation="relu")(net_states)
+        net_states = layers.Dense(units=64, kernel_regularizer=regularizers.l2(0.01))(
+            net_states
+        )
         net_states = layers.BatchNormalization()(net_states)
         net_states = layers.Activation("relu")(net_states)
 
         # Add hidden layer(s) for action pathway
-        net_actions = layers.Dense(units=32, activation="relu")(actions)
+        net_actions = layers.Dense(units=32, kernel_regularizer=regularizers.l2(0.01))(
+            actions
+        )
         net_actions = layers.BatchNormalization()(net_actions)
         net_actions = layers.Activation("relu")(net_actions)
-        net_actions = layers.Dense(units=64, activation="relu")(net_actions)
+        net_actions = layers.Dense(units=64, kernel_regularizer=regularizers.l2(0.01))(
+            net_actions
+        )
         net_actions = layers.BatchNormalization()(net_actions)
         net_actions = layers.Activation("relu")(net_actions)
 
